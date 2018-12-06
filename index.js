e06d835ebdbedbdbc0de380648b6334791e59e36
elraphty <elraphtyofficial@gmail.com>
2018-12-06 01:56:41 +0100

    adding url module

--- index.js
+++ index.js
@@ -5,10 +5,21 @@
 
 // Dependencies
 const http  = require('http');
+const url = require('url');
 
  // Configure the server to respond to all requests with a string
 const server = http.createServer(function(req, res)  {
+  // Parse the url
+  let parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
   res.end('Hello World!\n');
+
+  // Log the request/response
+  console.log('Request received on path: '+trimmedPath)
 });
 
 // Start the server
