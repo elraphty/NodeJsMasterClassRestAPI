7f8353f44cd345ec0c23d844f7d8af02c57c29e1
elraphty <elraphtyofficial@gmail.com>
2018-12-07 15:29:28 +0100

    adding routing

--- index.js
+++ index.js
@@ -33,17 +33,50 @@ const server = http.createServer(function(req, res)  {
   req.on('data', function(data) {
       buffer += decoder.write(data);
   });
+  
   req.on('end', function() {
       buffer += decoder.end();
 
-      // Send the response
-      res.end('Hello World!\n');
+      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+      let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-      // Log the request/response
-      console.log('Request received with this payload: ',buffer);
+       // Construct the data object to send to the handler
+       let data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : buffer
+      };
+
+      // // Send the response
+      // res.end('Hello World!\n');
+
+      // // Log the request/response
+      // console.log('Request received with this payload: ', buffer);
       
-      // Log the request/response
-      console.log('Request received on path: '+trimmedPath+' with method: '+method+' and this query string: ',queryStringObject,  headers, buffer);
+      // // Log the request/response
+      // console.log('Request received on path: '+trimmedPath+' with method: '+method+' and this query string: ', queryStringObject,  headers, buffer);
+
+      // Route the request to the handler specified in the router
+      chosenHandler(data, function(statusCode, payload){
+
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
+
+        // Convert the payload to a string
+        let payloadString = JSON.stringify(payload);
+
+        // Return the response
+        res.writeHead(statusCode);
+        res.end(payloadString);
+        console.log("Returning this response: ", statusCode,payloadString);
+
+      });
+
   });
 
 });
@@ -52,3 +85,21 @@ const server = http.createServer(function(req, res)  {
 server.listen(3000, function() {
   console.log('The server is up and running now');
 });
+
+// Define all the handlers
+let handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+  callback(406, {'name': 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data, callback){
+  callback(404);
+};
+
+// Define the request router
+let router = {
+  'sample' : handlers.sample
+};
